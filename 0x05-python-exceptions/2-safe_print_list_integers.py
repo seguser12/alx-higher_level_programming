d3f4c077695421e66c361e58ae07c917556f307f
Lurldgbodex <gbodisegun@gmail.com>
2022-12-20 00:21:05 +0100

    update variable name

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     no_of_printed_elements = 0
-    for idx in range(0, x):
+    for i in range(0, x):
         try:
-            print("{:d}". format(my_list[idx]), end='')
+            print("{:d}". format(my_list[i]), end='')
             no_of_printed_elements += 1
         except (ValueError, TypeError):
             pass
