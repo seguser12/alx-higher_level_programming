485bf78f7e3130e43083be697995397a7fb83df6
seguser <gbodisegun@gmail.com>
2022-12-20 10:51:30 -0800

    add file 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" square definition """
+
+
+class Square:
+    '''square class'''
+    def __init__(self, size=0):
+        '''class initialization
+        Args:
+            size: size o the square
+        '''
+        self.__size = size
+
+    @property
+    def size(self):
+        '''get current size of square'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        '''instance method that returns current square area'''
+        return self.__size ** 2
+
+    def my_print(self):
+        '''prints the square to stdout with character #'''
+        if self.__size == 0:
+            print()
+        for row in range(0, self.__size):
+            for col in range(self.__size):
+                print("#", end='')
+            print()
