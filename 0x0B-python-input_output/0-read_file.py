e3358afa9828e659f8a2fed5b81e25ff3151270e
seguser <gbodisegun@gmail.com>
2023-01-10 11:21:29 -0800

    format outpu

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     '''reads a text file and prints it to stdout'''
     with open(filename, encoding='UTF-8') as f:
-        print(f.read())
+        print(f.read(), end='')
